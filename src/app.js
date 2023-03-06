f5ca3aeff320bbc51e3239cb4249bebcf5014ef5
Sonanielectronics <100422141+Sonanielectronics@users.noreply.github.com>
2023-03-06 16:41:18 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,10 +7,14 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 
+// creating 24 hours from milliseconds
+const oneDay = 1000 * 60 * 60 * 24;
+
 const sessions = require('express-session');
 app.use(sessions({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
     saveUninitialized:true,
+    cookie: { maxAge: oneDay },
     resave: false 
 }));
 
