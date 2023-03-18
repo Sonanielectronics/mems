d525338fcea567af61e49c4d19fba388003fe057
Sonanielectronics <100422141+Sonanielectronics@users.noreply.github.com>
2023-03-18 16:50:01 +0530

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -6,21 +6,23 @@ var path = require("path");
 
 var { upload } = require("../middleware/schema");
 
-router.get("/Registration", class1.a);
-router.post("/Registration", upload.single("add2"), class1.b);
+router.get("/", class1.a);
 
-router.get("/login", class1.c);
-router.post("/login", class1.d);
+router.get("/Registration", class1.b);
+router.post("/Registration", upload.single("add2"), class1.c);
 
-router.get("/upload", class1.e);
-router.post("/upload", upload.array("add2", 100), class1.f);
+router.get("/login", class1.d);
+router.post("/login", class1.e);
 
-router.get("/delete", class1.g);
+router.get("/upload", class1.f);
+router.post("/upload", upload.array("add2", 100), class1.g);
 
-router.get("/:id", class1.h);
+router.get("/delete", class1.h);
 
-router.get("/:id1/:id2", class1.i);
+router.get("/:id", class1.i);
 
-router.get("/:id1/:id2/:id3", class1.j);
+router.get("/:id1/:id2", class1.j);
+
+router.get("/:id1/:id2/:id3", class1.k);
 
 module.exports = router;
